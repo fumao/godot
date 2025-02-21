fa4223a991fb03e71890aeca897ff3416cd041e1
fumao <fumao8521@126.com>
2025-02-21 16:21:24 +0800

    Close the editor object and arbitrarily modify the setting _edited true logic

--- core/object/object.cpp
+++ core/object/object.cpp
@@ -256,10 +256,10 @@ void Object::_get_valid_parents_static(List<String> *p_parents) {
 }
 
 void Object::set(const StringName &p_name, const Variant &p_value, bool *r_valid) {
-#ifdef TOOLS_ENABLED
+// #ifdef TOOLS_ENABLED
 
-	_edited = true;
-#endif
+// 	_edited = true;
+// #endif
 
 	if (script_instance) {
 		if (script_instance->set(p_name, p_value)) {
